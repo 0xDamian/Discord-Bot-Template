c55de5f509740c0bd006fd99425faac3f8c93d39
0xDamian <99565245+0xDamian@users.noreply.github.com>
2022-09-28 10:11:53 -0400

    Create error.py

---
+++ cogs/error.py
@@ -0,0 +1,32 @@
+import discord
+from discord.ext import commands
+
+
+class ErrorCog(commands.Cog, name='Error'):
+
+    def __init__(self, client):
+
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_command_error(self, ctx, error):
+        try:
+            if hasattr(ctx.command, 'on_error'):
+                return
+            else:
+                embed = discord.Embed(
+                    title=f'Error in {ctx.command}:',
+                    description=
+                    f'```{ctx.command.qualified_name} {ctx.command.signature} \n{error}```',
+                    colour=0x4378)
+                await ctx.send(embed=embed)
+        except:
+            embed = discord.Embed(title=f'Error in {ctx.command}:',
+                                  description=f'```{error}```',
+                                  colour=0x4378)
+            await ctx.send(embed=embed)
+
+
+async def setup(client):
+    await client.add_cog(ErrorCog(client))
+    print("Error Loaded")
